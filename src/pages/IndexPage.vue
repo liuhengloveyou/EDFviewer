2c00fb98adbe21fc606192f3ba6d65cae83dcde2
heng liu <35221199@qq.com>
2024-03-27 07:29:30 +0100

    init

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,84 @@
+<template>
+  <q-page class="flex flex-center">
+    <q-header>
+      <q-bar>
+        <q-icon name="web" />
+        <div>EDFViewer</div>
+
+        <q-space />
+
+        <!-- <q-btn dense flat icon="minimize" />
+        <q-btn dense flat icon="crop_square" />
+        <q-btn dense flat icon="close" /> -->
+      </q-bar>
+
+      <div class="q-pa-sm q-pl-md row items-center">
+        <div class="cursor-pointer non-selectable">
+          File
+          <q-menu>
+            <q-list dense style="min-width: 100px">
+              <q-item clickable v-close-popup>
+                <q-item-section>Open...</q-item-section>
+              </q-item>
+              <q-item clickable v-close-popup>
+                <q-item-section>New</q-item-section>
+              </q-item>
+            </q-list>
+          </q-menu>
+        </div>
+
+        <div class="q-ml-md cursor-pointer non-selectable">
+          Edit
+          <q-menu auto-close>
+            <q-list dense style="min-width: 100px">
+              <q-item clickable>
+                <q-item-section>Cut</q-item-section>
+              </q-item>
+              <q-item clickable>
+                <q-item-section>Copy</q-item-section>
+              </q-item>
+              <q-item clickable>
+                <q-item-section>Paste</q-item-section>
+              </q-item>
+              <q-separator />
+              <q-item clickable>
+                <q-item-section>Select All</q-item-section>
+              </q-item>
+            </q-list>
+          </q-menu>
+        </div>
+      </div>
+    </q-header>
+
+    <canvas class="emscripten" id="canvas" oncontextmenu="event.preventDefault()"></canvas>
+  </q-page>
+</template>
+
+<script setup>
+defineOptions({
+  name: 'IndexPage'
+});
+</script>
+
+<style scoped>
+.emscripten {
+  position: absolute;
+  top: 0px;
+  left: 0px;
+  margin: 0px;
+  padding: 0px;
+  border: 0;
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+  display: block;
+  image-rendering: optimizeSpeed;
+  image-rendering: -moz-crisp-edges;
+  image-rendering: -o-crisp-edges;
+  image-rendering: -webkit-optimize-contrast;
+  image-rendering: optimize-contrast;
+  image-rendering: crisp-edges;
+  image-rendering: pixelated;
+  -ms-interpolation-mode: nearest-neighbor;
+}
+</style>
